bd01001bd2303c3980fec3c84e07cef74620cec6
razakhan3 <raza@pensil.in>
2023-07-21 12:05:10 +0530

    Added routes and controllers connected mongo db added users and course schema separetely

--- app.js
+++ app.js
@@ -8,5 +8,7 @@ const app = express();
 
 // Importing & using Routes
 import course from "./routes/courseRoutes.js";
+import user from "./routes/userRoutes.js";
 app.use("/api/v1", course);
+app.use("/api/v1", user);
 export default app;
