62f032b24b9c2b6e67733e0d90e2bbc49f061e14
raza <razakhanha05@gmail.com>
2023-07-22 14:07:37 +0530

    Added routes and controller for createCourse Added separate Error handler Added catchAsyncError (dynamic)

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import express from "express";
 import { config } from "dotenv";
+import ErrorMiddleware from "./middlewares/Error.js";
 
 config({
   path: "./config/config.env",
 });
 const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // Importing & using Routes
 import course from "./routes/courseRoutes.js";
 import user from "./routes/userRoutes.js";
+
 app.use("/api/v1", course);
 app.use("/api/v1", user);
 export default app;
+
+app.use(ErrorMiddleware);
