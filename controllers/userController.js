a1f70b3eaf6820e592ae40d1fefa65885ca7cdf2
raza <razakhanha05@gmail.com>
2023-07-23 16:40:01 +0530

    Added routes and controllers for -changepassword -resetpassword -forgetpassword -updateprofile Added sendMail utils Added getResetToken for auth (used crypto) Added auth middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { User } from "../models/User.js";
 import { sendToken } from "../utils/sendToken.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import crypto from "crypto";
 
 export const register = catchAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -63,11 +65,100 @@ export const logout = catchAsyncError(async (req, res, next) => {
 
 export const getMyProfile = catchAsyncError(async (req, res, next) => {
   // Find the user by their ID (assuming it's stored in req.user._id)
-  const user = User.findById(req.user._id);
+  const user = await User.findById(req.user._id);
 
   // Return the user's profile in the response
   res.status(200).json({
     success: true,
     user,
+  });
+});
+
+export const changePassword = catchAsyncError(async (req, res, next) => {
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword)
+    return next(new ErrorHandler("Please enter all fields", 400));
+
+  const user = await User.findById(req.user._id).select("+password");
+
+  const isMatched = await user.comparePassword(oldPassword);
+
+  if (!isMatched) return next(new ErrorHandler("Incorrect old password", 400));
+
+  user.password = newPassword;
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Password changed successfully!",
+  });
+});
+
+export const updateProfile = catchAsyncError(async (req, res, next) => {
+  const { name, email } = req.body;
+
+  const user = await User.findById(req.user._id);
+
+  if (name) user.name = name;
+  if (email) user.email = email;
+
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Profile updated successfully!",
+  });
+});
+
+export const forgetPassword = catchAsyncError(async (req, res, next) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) return next(new ErrorHandler("User not found", 400));
+
+  const resetToken = await user.getResetToken();
+
+  await user.save();
+
+  const url = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
+
+  const message = `Click on the link to reset your password. ${url}. If you have not requested then please ignore.`;
+
+  // Send token via email
+  await sendEmail(user.email, "Fightclub Reset Password", message);
+
+  res.status(200).json({
+    success: true,
+    message: `Reset Token has been sent to ${user.email}`,
+  });
+});
+
+export const resetPassword = catchAsyncError(async (req, res, next) => {
+  const { token } = req.params;
+
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: {
+      $gt: Date.now(),
+    },
+  });
+  if (!user)
+    return next(new ErrorHandler("Token is invalid or has been expired", 400));
+
+  user.password = req.body.password;
+  user.resetPasswordExpire = undefined;
+  user.resetPasswordToken = undefined;
+
+  await user.save();
+  res.status(200).json({
+    success: true,
+    message: "Profile changed successfully!",
   });
 });
