f12eb4ab6f44d11167525c0dd0980ace468dc328
raza <razakhanha05@gmail.com>
2023-07-23 11:28:02 +0530

    Added controller and routes for -login -logout -getProfile(me) also added method in USer model -Made jwt token secure false for now will change it later

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,15 +6,15 @@ import { sendToken } from "../utils/sendToken.js";
 export const register = catchAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
 
-  //   const file = req.file;
+  // Check if all required fields are provided
   if (!name || !email || !password)
     return next(new ErrorHandler("Please enter all fields", 400));
 
+  // Check if user already exists
   let user = await User.findOne({ email });
-  if (user) return next(new ErrorHandler("User already exist"), 409);
+  if (user) return next(new ErrorHandler("User already exists", 409));
 
-  //upload file on cloudnary
-
+  // Create the user with default avatar values
   user = await User.create({
     name,
     email,
@@ -25,5 +25,49 @@ export const register = catchAsyncError(async (req, res, next) => {
     },
   });
 
-  sendToken(res, user, "Registered succesfully", 201);
+  // Send token in response upon successful registration
+  sendToken(res, user, "Registered successfully", 201);
+});
+
+export const login = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return next(new ErrorHandler("Please enter all fields", 400));
+
+  // Find the user by email (selecting password field)
+  const user = await User.findOne({ email }).select("+password");
+
+  // Check if the user exists
+  if (!user) return next(new ErrorHandler("Incorrect email or password", 401));
+
+  const isMatched = await user.comparePassword(password);
+  if (!isMatched)
+    return next(new ErrorHandler("Incorrect email or password", 401));
+
+  // Send token in response upon successful login
+  sendToken(res, user, `Welcome back ${user.name}`, 200);
+});
+
+export const logout = catchAsyncError(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", null, {
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "Logged out successfully",
+    });
+});
+
+export const getMyProfile = catchAsyncError(async (req, res, next) => {
+  // Find the user by their ID (assuming it's stored in req.user._id)
+  const user = User.findById(req.user._id);
+
+  // Return the user's profile in the response
+  res.status(200).json({
+    success: true,
+    user,
+  });
 });
