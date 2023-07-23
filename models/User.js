823e22a7ce39b82ce5fc7602a4e2698405c3f2ea
raza <razakhanha05@gmail.com>
2023-07-23 16:40:01 +0530

    Added routes and controllers for -changepassword -resetpassword -forgetpassword -updateprofile Added sendMail utils Added getResetToken for auth (used crypto) Added auth middleware

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import validator from "validator";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 const schema = new mongoose.Schema({
   name: {
@@ -78,7 +79,19 @@ schema.methods.getJWTToken = function () {
 
 // Method to compare the provided password with the hashed password in the database
 schema.methods.comparePassword = async function (password) {
-  console.log(this.password);
   return await bcrypt.compare(password, this.password);
+};
+
+schema.methods.getResetToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+
+  return resetToken;
 };
 export const User = mongoose.model("User", schema);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,40 @@
 import express from "express";
 import {
+  changePassword,
+  forgetPassword,
   getMyProfile,
   login,
   logout,
   register,
+  resetPassword,
+  updateProfile,
 } from "../controllers/userController.js";
+import { isAuthenticated } from "../middlewares/Auth.js";
 
 const router = express.Router();
+
 //to register a new user
 router.route("/register").post(register);
+
 //login
 router.route("/login").post(login);
+
 //logout
 router.route("/logout").get(logout);
+
 //get my profile
-router.route("/me").get(getMyProfile);
+router.route("/me").get(isAuthenticated, getMyProfile);
+
+//change password
+router.route("/changepassword").put(isAuthenticated, changePassword);
+
+//updateProfile
+router.route("/updateprofile").put(isAuthenticated, updateProfile);
+
+//updateProfile
+router.route("/forgetpassword").post(forgetPassword);
+
+//updateProfile
+router.route("/resetpassword/:token").put(resetPassword);
 
 export default router;
