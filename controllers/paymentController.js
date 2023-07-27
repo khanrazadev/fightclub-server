ce77666be2493e59797a2104cbb9dfcaa7d9c628
Raza Khan <63768854+razakhan05@users.noreply.github.com>
2023-07-27 22:25:32 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -35,13 +35,13 @@ export const paymentVerification = catchAsyncError(async (req, res, next) => {
   const { razorpay_signature, razorpay_payment_id, razorpay_subscription_id } =
     req.body;
 
-  const user = await User.findById(req.user._id);
+  // const user = await User.findById(req.user._id);
 
-  const subscription_id = user.subscription.id;
+  // const subscription_id = user.subscription.id;
 
   const generated_signature = crypto
     .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
-    .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
+    .update(razorpay_payment_id + "|" +razorpay_subscription_id)
     .digest("hex");
 
   const isAuthentic = generated_signature === razorpay_signature;
