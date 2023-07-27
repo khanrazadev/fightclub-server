c9d14262302da52f498b52b1324993f76b40168d
Raza Khan <63768854+razakhan05@users.noreply.github.com>
2023-07-28 00:47:58 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -34,10 +34,10 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
 
 // Method to handle payment verification after a successful subscription purchase
 export const paymentVerification = catchAsyncError(async (req, res, next) => {
-  const { razorpay_signature, razorpay_payment_id, razorpay_subscription_id } =
-    req.body;
+  // const { razorpay_signature, razorpay_payment_id, razorpay_subscription_id } =
+  //   req.body;
 
-  const user = await User.findById(req.user?._id);
+  const user = await User.findById(req.user._id);
 
   // const subscription_id = user.subscription.id;
 
@@ -52,19 +52,19 @@ export const paymentVerification = catchAsyncError(async (req, res, next) => {
   //   return res.redirect(`${process.env.FRONTEND_URL}/paymentfail`);
 
   // database comes here
-  await Payment.create({
-    razorpay_signature,
-    razorpay_payment_id,
-    razorpay_subscription_id,
-  });
+  // await Payment.create({
+  //   razorpay_signature,
+  //   razorpay_payment_id,
+  //   razorpay_subscription_id,
+  // });
 
   user.subscription.status = "active";
 
   await user.save();
 
-  res.redirect(
-    `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
-  );
+  // res.redirect(
+  //   `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
+  // );
 });
 
 export const getRazorPayKey = catchAsyncError(async (req, res, next) => {
