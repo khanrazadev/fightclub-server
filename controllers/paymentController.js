ce025dc94da6700d00969cb3a3404db0a9ca4ee5
Raza Khan <63768854+razakhan05@users.noreply.github.com>
2023-07-28 01:54:37 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -35,14 +35,19 @@ export const buySubscription = catchAsyncError(async (req, res, next) => {
 });
 
 
+
 // Method to handle payment verification after a successful subscription purchase
 export const paymentVerification = catchAsyncError(async (req, res, next) => {
   const { razorpay_signature, razorpay_payment_id, razorpay_subscription_id } =
     req.body;
 
+  const user = await User.findById(req.user._id);
+
+  const subscription_id = user.subscription.id;
+
   const generated_signature = crypto
     .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
-    .update(razorpay_payment_id + "|" + razorpay_subscription_id)
+    .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
     .digest("hex");
 
   const isAuthentic = generated_signature === razorpay_signature;
@@ -56,6 +61,10 @@ export const paymentVerification = catchAsyncError(async (req, res, next) => {
     razorpay_payment_id,
     razorpay_subscription_id,
   });
+
+  user.subscription.status = "active";
+
+  await user.save();
 
   res.redirect(
     `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
