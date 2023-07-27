6bec15ed48d85d8c1d568fd4c98a0e2c76ff2986
Raza Khan <63768854+razakhan05@users.noreply.github.com>
2023-07-28 01:08:05 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -54,12 +54,12 @@ export const paymentVerification = catchAsyncError(async (req, res, next) => {
   // if (!isAuthentic)
   //   return res.redirect(`${process.env.FRONTEND_URL}/paymentfail`);
 
-  // database comes here
-  // await Payment.create({
-  //   razorpay_signature,
-  //   razorpay_payment_id,
-  //   razorpay_subscription_id,
-  // });
+  database comes here
+  await Payment.create({
+    razorpay_signature,
+    razorpay_payment_id,
+    razorpay_subscription_id,
+  });
 
   user.subscription.status = "active";
 
